c177d8520d563e1150bf39507112e670fb1c1c76
unknown <sulavniroula78@gmail.com>
2022-07-06 12:32:14 +0545

    updated GFG,Practice and Students

--- Students.java
+++ Students.java
@@ -2,10 +2,13 @@ import java.util.Random;
 
 public class Students {
         public static void oneTimePad() {
+                int st = 45;
+                System.out.println(st);
                 Random ran = new Random();
                 String s = "0123456789ABCDEF";
                 for (int i = 0; i < 100; i++) {
                         System.out.print(s.charAt(ran.nextInt(s.length())));
                 }
         }
+
 }
